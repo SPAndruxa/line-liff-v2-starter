109de7a128e6ae5b7af81e6470501259e6319207
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-03 20:18:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+let request = require('request');
 
 app.use(express.static('public'));
 

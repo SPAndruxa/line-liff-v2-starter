cb01819f2bca1adbf1c1d7ac5fea26611edc37fc
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-03 19:53:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,7 @@ let request = require('request');
 app.use(express.static('public'));
 
 app.get('/send-id', function(req, res) {
-    let options = {
-        'method': 'POST',
-        'url': 'https://www.corezoid.com/api/1/json/public/891766/d762277ea79fc5652f7166e51412768cd2e28928',
-        'headers': {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id: myLiffId})
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        res.json({id: myLiffId});
-    });
+    res.json({id: myLiffId});
 });
-
-
-
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));

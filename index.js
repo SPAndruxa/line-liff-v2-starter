1041f55dfa12560264ca185b9ed3e98bd32609b7
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-03 20:35:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
-let request = require('request');
 
 app.use(express.static('public'));
-
-app.get('/send-corezoid', function(req, res) {
-    request(getOptions(req), function (error, response) {
-        res.json({result: "success"});
-    });
-});
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
@@ -18,14 +11,3 @@ app.get('/send-id', function(req, res) {
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
 
-function getOptions(body){
-    let options = {
-        'method': 'POST',
-        'url': 'https://www.corezoid.com/api/1/json/public/891766/d762277ea79fc5652f7166e51412768cd2e28928',
-        'headers': {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    }
-    return options;
-}

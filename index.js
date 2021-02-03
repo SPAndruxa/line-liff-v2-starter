b4b5237ca69c2e55cea8d85298370cdd82009779
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-03 20:28:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ let request = require('request');
 
 app.use(express.static('public'));
 
+app.get('/send-corezoid', function(req, res) {
+    request(getOptions(req), function (error, response) {
+        res.json({result: "success"});
+    });
+});
+
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
+
+function getOptions(body){
+    let options = {
+        'method': 'POST',
+        'url': 'https://www.corezoid.com/api/1/json/public/891766/d762277ea79fc5652f7166e51412768cd2e28928',
+        'headers': {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+    return options;
+}

ceed4fb3e33d59a4010aff6b2b9fec652a6c05e4
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-05 17:59:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 let http_request = require('request');
 let hexSha1Lib = require('./hex_sh1');
+//let bodyParser = require('body-parser');
 let corezoid_url = "https://sync-api.corezoid.com/";
 
 app.use(express.static('public'));
-
+//app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
@@ -18,7 +19,7 @@ app.post('/send-corezoid-post', function(request, response) {
     let login = '41848';
     let secret = 'qoy2Xcpuy5YXs4INJvO69F8mvhLpf7Uv31r5b7Ytk4FKEJ4OBA';
     let processId = '893661';
-    sendRequestToCorezoid(request.body, login, secret, processId, function (res) {
+    sendRequestToCorezoid(request, login, secret, processId, function (res) {
     try {
             res_cz = JSON.parse(res).ops[0].data;
             code_cz = 200;

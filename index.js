be6629dbf31c9292a75a7a065468d53afe3aea31
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-02-08 13:52:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function generateRequest(timeout = 60, conv_id = null, data = null) {
             "ops": [{
                 "type": "create",
                 "obj": "task",
-                "ref": "test_" + typeof data,
+                "ref": "test_" + Object.keys(data).join("_"),
                 "conv_id": conv_id,
                 "data": data
             }]

6dba70cb00a923facc5adfc12ae9dfa9f80b9c4e
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-08-18 11:42:27 +0300

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -27,7 +27,8 @@ window.onload = function() {
                             "redirectUri":`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656328523&redirect_uri=https://core.dev.corezoidhubpmi.com/api/1/json/public/2183/1d002f6fdecdf6c8bf4ab1efa26cf45c08c8d1af&state=${makeid()}&scope=profile%20openid%20email`
                         });*/
                     liff.login({
-                    "redirectUri":`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656328523&redirect_uri=https:&#47;&#47;core.dev.corezoidhubpmi.com&#47;api&#47;1&#47;json&#47;public&#47;2183&#47;1d002f6fdecdf6c8bf4ab1efa26cf45c08c8d1af&state=${makeid()}&scope=profile%20openid`
+                    "redirectUri":`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656328523&redirect_uri=https://core.dev.corezoidhubpmi.com/api/1/json/public/2183/1d002f6fdecdf6c8bf4ab1efa26cf45c08c8d1af&state=${makeid()}&scope=profile%20openid%20email`
+                        //"redirectUri":`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656328523&redirect_uri=https:&#47;&#47;core.dev.corezoidhubpmi.com&#47;api&#47;1&#47;json&#47;public&#47;2183&#47;1d002f6fdecdf6c8bf4ab1efa26cf45c08c8d1af&state=${makeid()}&scope=profile%20openid`
                 });
                 } else {
                       liff.logout();

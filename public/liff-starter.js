67f275da61bd914ebe26495ab84ea687d8373252
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-08-20 12:56:38 +0300

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,1 +1,55 @@
+window.onload = function() {
+    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
 
+    // DO NOT CHANGE THIS
+    let myLiffId = "";
+
+    // if node is used, fetch the environment variable and pass it to the LIFF method
+    // otherwise, pass defaultLiffId
+    if (useNodeJS) {
+        fetch('/send-id')
+            .then(function(reqResponse) {
+                return reqResponse.json();
+            })
+            .then(function(jsonResponse) {
+                myLiffId = jsonResponse.id;
+                initializeLiffOrDie(myLiffId);
+            })
+            .catch(function(error) {
+                alert("Error init");
+            });
+    } else {
+        myLiffId = defaultLiffId;
+        initializeLiffOrDie(myLiffId);
+    }
+};
+
+function initializeLiffOrDie(myLiffId) {
+    if (!myLiffId) {
+
+    } else {
+        initializeLiff(myLiffId);
+    }
+}
+
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            // start to use LIFF's api
+            //initializeApp();
+        })
+        .catch((err) => {
+            alert("Error initializeApp")
+        });
+}
+
+document.getElementById('liffLoginButton').addEventListener('click', function() {
+        if (!liff.isLoggedIn()) {
+            // set `redirectUri` to redirect the user to a URL other than the front page of your LIFF app.
+            liff.login();
+        }
+    });

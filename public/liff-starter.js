fd1c6a5bfad848a98424c8a44f7385e7a84b4ddb
Chaban Andrey <45711642+SPAndruxa@users.noreply.github.com>
2021-09-03 17:12:44 +0300

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -26,48 +26,40 @@ window.onload = function() {
 };
 
 function initializeLiffOrDie(myLiffId) {
-    if (!myLiffId) {
-
-    } else {
+    if (myLiffId) {
         initializeLiff(myLiffId);
     }
 }
 
 function initializeLiff(myLiffId) {
-    liff
-        .init({
-            liffId: myLiffId
-        })
-        .then(() => {
-            if (!liff.isLoggedIn()) {
-                liff.login();
-            } else {
-                //document.getElementById("regOrLogin").hidden = false;                
-                liff.getProfile().then(profile => {
-                    userId = profile.userId;
-                    fetch('/send-sync-corezoid', {
-                        method:"POST",
-                        headers: {
-                          'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(profile)
-                    }).then(function(reqResponse) {
-                        return reqResponse.json();
-                    }).then(function(jsonResponse) {
-                        console.log(jsonResponse)
-                        checkTypeAndGoNextStep(jsonResponse);
-                    }).catch(function(error) {
-                        console.log(error)
-                    });
-                    ///
-                }).catch((err) => {
-                  console.log('error', err);
+    liff.init({
+        liffId: myLiffId
+    }).then(() => {
+        if (!liff.isLoggedIn()) {
+            liff.login();
+        } else {              
+            liff.getProfile().then(profile => {
+                userId = profile.userId;
+                fetch('/send-sync-corezoid', {
+                    method:"POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(profile)
+                }).then(function(reqResponse) {
+                    return reqResponse.json();
+                }).then(function(jsonResponse) {
+                    checkTypeAndGoNextStep(jsonResponse);
+                }).catch(function(error) {
+                    console.log(error)
                 });
-            }
-        })
-        .catch((err) => {
-            alert("Error initializeApp")
-        });
+            }).catch((err) => {
+                console.log('error', err);
+            });
+        }
+    }).catch((err) => {
+        alert("Error initializeApp")
+    });
 }
 
 function redirectOnUrl(url){
@@ -83,7 +75,6 @@ function showScreen(startScreen){
     startScreen:startScreen,
     onAfterSubmit:function(e){
         delete e.response.requestParams.customLang;
-        console.log(e)
         
         e.chat_id_hax = urlParams.id;
         e.sup = urlParams.sup;
@@ -94,200 +85,16 @@ function showScreen(startScreen){
               'Content-Type': 'application/json'
             },
             body: JSON.stringify(e)
-        }).then(function(reqResponse) {
-            return reqResponse.json();
-        }).then(function(jsonResponse) {
-            if(jsonResponse.screen === "Registration_Web_LINE"){
-                //document.getElementById("regOrLogin").hidden = false;
-                //document.getElementById("resultStat").hidden = false;
-                document.getElementById("contentText").innerHTML = `An email has been sent to your mail ${jsonResponse.response.user.email} to complete the registration. Please complete the registration.`;
-            } else if (jsonResponse.screen === "Login_Web"){
-    ///////////////////
-//     fetch('/log', {
-//         method:"POST",
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({res:jsonResponse.response.UID})
-//     });
-    ////////////////////
-                fetch('/get-user-profile', {
-                    method:"POST",
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({UID:jsonResponse.response.UID})
-                }).then(function(reqResponse) {
-                    return reqResponse.json();
-                }).then(function(jsonResponse) {
-                    var hasHav = false;
-                    if(jsonResponse.data.hasOwnProperty("hardAV")){
-                        if(jsonResponse.data.hardAV.some(hav => hav.status_refcode === "VERIFIED")){
-                            fetch('/send-corezoid-webhook', {
-                                method:"POST",
-                                headers: {
-                                  'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify({
-                                    chat_id_hax: urlParams.id,
-                                    sup: urlParams.sup,
-                                    userId: userId,
-                                    screen: "hav"
-                                })
-                            }).then(function(jsonResponse) {
-                                redirectOnUrl(jsonResponse.botLink);
-                            }).catch(function(error) {
-                                alert("Error init");
-                            });
-                            hasHav = true;
-                            
-                        }
-                    }
-                    if(!hasHav){
-                        document.getElementById("regOrLogin").hidden = true;
-                        document.getElementById("formRedirect").hidden = false;
-                        (function myLoop(i) {
-                            setTimeout(function () {
-                                console.log();
-                                document.getElementById("timer").innerHTML = i;
-                                if (--i >= 0) {
-                                    myLoop(i);
-                                } else {
-                                    redirectOnUrl(jsonResponse.havVerify);
-                                }      //  decrement i and call myLoop again if i > 0
-                            }, 1000);
-                        })(6 - 1);
-                    }
-                }).catch(function(error) {
-                    alert("error - ", error)
-                });
-            }
-            console.log(jsonResponse)
-        }).catch(function(error) {
-            console.log(error)
         });
+        if(e.response.status === "OK"){
+            redirectOnUrl("https://line.me/R/ti/p/@579psxyw?from=page");
+        }
     }
   });
 }
 
 function checkTypeAndGoNextStep(data) {
-    if (data.type === "nothing" || data.type === "loggedin") {
-        document.getElementById("wait").hidden = true;
-        document.getElementById("regOrLogin").hidden = false;
-        document.getElementById("contentText").innerHTML = data.body;
-    } else if (data.type === "verified"){
-        fetch('/get-user-profile', {
-            method:"POST",
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({UID:data.userProfile.guid})
-        }).then(function(reqResponse) {
-            return reqResponse.json();
-        }).then(function(jsonResponse) {
-            document.getElementById("wait").hidden = true;
-            document.getElementById("regOrLogin").hidden = false;
-            if(jsonResponse.isVerified){
-                fetch('/send-corezoid-webhook', {
-                    method:"POST",
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        chat_id_hax: urlParams.id,
-                        sup: urlParams.sup,
-                        userId: userId,
-                        screen: "verified"
-                    })
-                });
-                document.getElementById("contentText").innerHTML = data.alterBody;
-            } else {
-                document.getElementById("contentText").innerHTML = data.body;
-            }
-        }).catch(function(error) {
-            alert("error - ", error)
-        });
-    } else if (data.type === "hav") {
-        //////////////////////////////////////////
-        fetch('/get-user-profile', {
-            method:"POST",
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({UID:data.userProfile.guid})
-        }).then(function(reqResponse) {
-            return reqResponse.json();
-        }).then(function(jsonResponse) {
-            var hasHav = false;
-            if(jsonResponse.data.hasOwnProperty("hardAV")){
-                if(jsonResponse.data.hardAV.some(hav => hav.status_refcode === "VERIFIED")){
-                    fetch('/send-corezoid-webhook', {
-                        method:"POST",
-                        headers: {
-                          'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            chat_id_hax: urlParams.id,
-                            sup: urlParams.sup,
-                            userId: userId,
-                            screen: "hav"
-                        })
-                    }).then(function(jsonResponse) {
-                        redirectOnUrl(jsonResponse.botLink);
-                    }).catch(function(error) {
-                        alert("Error init");
-                    });
-                    hasHav = true;
-                }
-            }
-            if(!hasHav){
-                document.getElementById("regOrLogin").hidden = true;
-                document.getElementById("formRedirect").hidden = false;
-                fetch('/send-corezoid-webhook', {
-                    method:"POST",
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        chat_id_hax: urlParams.id,
-                        sup: urlParams.sup,
-                        userId: userId,
-                        UID:data.userProfile.guid,
-                        screen: "waitHav"
-                    })
-                });
-                (function myLoop(i) {
-                    setTimeout(function () {
-                        console.log();
-                        document.getElementById("timer").innerHTML = i;
-                        if (--i >= 0) {
-                            myLoop(i);
-                        } else {
-                            redirectOnUrl(jsonResponse.havVerify);
-                        }      //  decrement i and call myLoop again if i > 0
-                    }, 1000);
-                })(6 - 1);
-            }
-        }).catch(function(error) {
-            alert("error - ", error)
-        });
-        //////////////////////////////////////////
-        
-//         document.getElementById("wait").hidden = true;
-//         document.getElementById("formRedirect").hidden = false;
-//         (function myLoop(i) {
-//             setTimeout(function () {
-//                 console.log();
-//                 document.getElementById("timer").innerHTML = i;
-//                 if (--i >= 0) {
-//                     myLoop(i);
-//                 } else {
-//                     console.log("END");
-//                     redirectOnUrl(data.url);
-//                 }      //  decrement i and call myLoop again if i > 0
-//             }, 1000);
-//         })(6 - 1);
-    } else if (data.type === "successful") {
+    if (data.type === "successful") {
         fetch('/send-corezoid-webhook', {
             method:"POST",
             headers: {
@@ -297,12 +104,13 @@ function checkTypeAndGoNextStep(data) {
                 chat_id_hax: urlParams.id,
                 sup: urlParams.sup,
                 userId: userId,
-                screen: "hav"
+                screen: "returnToBot"
             })
         });
-        redirectOnUrl(data.url);
+        redirectOnUrl("https://line.me/R/ti/p/@579psxyw?from=page");
     } else {
-        alert("Unknown error")
+        document.getElementById("wait").hidden = true;
+        document.getElementById("regOrLogin").hidden = false;
     }
 }
 
@@ -316,3 +124,13 @@ var urlParams = window.location.search.replace('?','').split('&').reduce(
 );
 
 let userId = "";
+
+    ///////////////////
+//     fetch('/log', {
+//         method:"POST",
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({res:jsonResponse.response.UID})
+//     });
+    ////////////////////
